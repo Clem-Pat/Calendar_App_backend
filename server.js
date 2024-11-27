7a213b4d8e325e90a73de1fe26c37ba875d7a274
Clément Patrizio <clement.patrizio@outlook.fr>
2024-11-27 11:56:02 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  host: 'autorack.proxy.rlwy.net',
+  port: 33328,
+  user: 'root',
+  password: 'VeucxKPpzypxltmUjplTqWuDoYoPOsih',
+  database: 'railway',
 });
 
 db.connect((err) => {
